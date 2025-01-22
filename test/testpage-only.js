ed69af772f5ef33cf97ed765b8a03603e371fa5f
animanman <103096854+animanman@users.noreply.github.com>
2025-01-23 03:39:08 +0900

    Update testpage-only.js

--- test/testpage-only.js
+++ test/testpage-only.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const footerDiv = document.querySelector('footer > div');
     const footerElement = document.querySelector('footer');
 
-    mainElement?.insertAdjacentHTML('afterbegin', '<div class="testpage_yome"><p>このページはあにまん検索【非公式】のテストページです<br />正規版は<a href="https://animanman.github.io/">こちら</a>からどうぞ</p></div>');
+    mainElement?.insertAdjacentHTML('afterbegin', '<div class="testpage_yome"><p>このページはあにまん検索【非公式】のテストページです。機能しない機能が沢山あります。<br />正規版は<a href="https://animanman.github.io/">こちら</a>からどうぞ</p></div>');
     footerDiv?.insertAdjacentHTML('beforeend', '<p><a href="https://github.com/animanman/animanman.github.io/tree/main/test">github(test)</a></p>');
 
 footerElement?.insertAdjacentHTML('afterend', `
