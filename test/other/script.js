5a0acd78c401a332f1be0ea91416602a74ec79b0
animanman <103096854+animanman@users.noreply.github.com>
2025-04-12 01:45:01 +0900

    Update script.js

--- test/other/script.js
+++ test/other/script.js
@@ -1,1 +1,21 @@
-
+Vue.createApp({
+      data() {
+        return {
+          // 各セクションごとに、headingとその中のスクリプト情報を格納
+          scriptGroups: [
+            {
+              heading: "tampermonkey用スクリプト",
+              scripts: [
+                {
+                  title: "下書き保存ツール",
+                  url: "https://animanman.github.io/test/other/anm-Draftsavingtool.js",
+                  description: "下書き保存を保存したり、書き込む時に安価先のレスを書き込みフォームの上に表示したりします"
+                }
+                // 他のスクリプト情報をここに追加可能
+              ]
+            }
+            // 他のグループがある場合は、ここにオブジェクトを追加
+          ]
+        }
+      }
+    }).mount("#otherapp")
