d823ce5835d699da5b6aeb4624e844424eb92b89
animanman <103096854+animanman@users.noreply.github.com>
2025-04-12 01:11:04 +0900

    Update script.js

--- commons/js/script.js
+++ commons/js/script.js
@@ -7,14 +7,14 @@ Vue.createApp({
           headerLinks: [
             { text: 'ホーム', url: '/' },
             { text: 'About', url: '/about' },
-            { text: 'サービス', url: '/services' },
-            { text: 'お問い合わせ', url: '/contact' }
+            { text: 'テスト版', url: '/test' },
+            { text: 'その他', url: '/test/other' },
+            { text: 'お問い合わせ', url: 'https://zawazawa.jp/animamman/topic/1' }
           ],
           // フッター用リンク配列（必要に応じて他の場所のリンクも追加）
           footerLinks: [
-            { text: 'プライバシーポリシー', url: '/privacy' },
-            { text: '利用規約', url: '/terms' },
-            { text: 'サイトマップ', url: '/sitemap' }
+            { text: 'animanman.github.io', url: '/' },
+            { text: 'github', url: 'https://github.com/animanman/animanman.github.io/' }
           ]
         }
       }
