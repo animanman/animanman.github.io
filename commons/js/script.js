a2e22cdbd4372228842bb6958920be6b8900368e
animanman <103096854+animanman@users.noreply.github.com>
2025-04-12 02:29:55 +0900

    Update script.js

--- commons/js/script.js
+++ commons/js/script.js
@@ -1,21 +1,29 @@
-Vue.createApp({
-      // Jekyllとの記法競合回避のためのデリミタ変更
-      delimiters: ['[[', ']]'],
-      data() {
-        return {
-          // ヘッダー用リンク配列
-          headerLinks: [
-            { text: 'ホーム', url: '/' },
-            { text: 'About', url: '/about' },
-            { text: 'テスト版', url: '/test' },
-            { text: 'その他', url: '/test/other' },
-            { text: 'お問い合わせ', url: 'https://zawazawa.jp/animamman/topic/1' }
-          ],
-          // フッター用リンク配列（必要に応じて他の場所のリンクも追加）
-          footerLinks: [
-            { text: 'animanman.github.io', url: '/' },
-            { text: 'github', url: 'https://github.com/animanman/animanman.github.io/' }
-          ]
-        }
-      }
-    }).mount('#templateapp')
+const headerapp = Vue.createApp({
+  data() {
+    return {
+      // 例：ヘッダー用のデータ
+      headerLinks: [
+        { text: 'ホーム', url: '/' },
+        { text: 'About', url: '/about' },
+        { text: 'テスト版', url: '/test' },
+        { text: 'その他', url: '/test/other' },
+        { text: 'お問い合わせ', url: 'https://zawazawa.jp/animamman/topic/1' }
+      ]
+    }
+  }
+});
+headerapp.mount('#headerapp');
+
+const footerapp = Vue.createApp({
+  data() {
+    return {
+      // 例：フッター用のデータ
+      footerLinks: [
+        { text: 'animanman.github.io', url: '/' },
+        { text: 'github', url: 'https://github.com/animanman/animanman.github.io/' }
+      ]
+    }
+  }
+});
+footerapp.mount('#footerapp');
+
