be6ff5bbf176c243fa015005d30219a5f3e63184
animanman <103096854+animanman@users.noreply.github.com>
2025-04-21 17:55:27 +0900

    Update script.js

--- contact/script.js
+++ contact/script.js
@@ -1,12 +1,7 @@
-// load イベント後に呼ぶのが安全
-  window.addEventListener('load', function() {
-    iFrameResize({
-      // 高さをどの要素基準で計算するか
-      heightCalculationMethod: 'bodyOffset',
-      // origin チェックを無効に（必要に応じて設定）
-      checkOrigin: false,
-      // ログを出したいとき
-      // log: true
-    }, '#InlineFrameContact');
-  });
+window.addEventListener('load', function() {
+  iFrameResize({
+    heightCalculationMethod: 'documentElementScroll', // または 'max'
+    checkOrigin: false
+  }, '#InlineFrameContact');
+});
 
