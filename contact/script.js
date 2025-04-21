2e6234433aa149aef6c7abde4b2f80d2a6fb8cab
animanman <103096854+animanman@users.noreply.github.com>
2025-04-21 11:18:22 +0900

    Update script.js

--- contact/script.js
+++ contact/script.js
@@ -1,1 +1,19 @@
+const postIframeHeight = () => {
+    const height = document.documentElement.offsetHeight;
+    window.parent.postMessage({ iframeHeight: height }, 'https://zawazawa.jp/animamman');
+};
 
+window.addEventListener('DOMContentLoaded', () => {
+    postIframeHeight();
+});
+
+window.addEventListener('resize', () => {
+    postIframeHeight();
+});
+
+window.addEventListener('message', (e) => {
+    if (e.data.hasOwnProperty('iframeHeight')) { 
+        const iframe = document.getElementById('InlineFrameContact'); // resizableIframeはiframeのID名に置き換える
+        iframe.style.height = `${e.data.iframeHeight}px`;
+    }
+});
