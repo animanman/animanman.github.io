6d29fc101fe3498a0aea11c15cd4e002011bd48d
animanman <103096854+animanman@users.noreply.github.com>
2025-04-21 19:15:23 +0900

    Update script.js

--- contact/script.js
+++ contact/script.js
@@ -1,2 +1,12 @@
+$(function(){
+    // 初期状態で非表示にしたい場合は外してもOK
+    $('#iframecontainer .iframe-container').hide();
 
-
+    // 折りたたみトグル
+    $('#iframeFolding').on('click', function(){
+      // this の次の .iframe-container を開閉
+      $(this).next('.iframe-container').slideToggle(300);
+      // （もし折りたたみ見出しの開閉状態をクラスで管理したい場合）
+      $(this).toggleClass('open');
+    });
+  });
