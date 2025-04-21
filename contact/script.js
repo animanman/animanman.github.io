3029564dd942f7aa64a9b2ee297096da4ede799f
animanman <103096854+animanman@users.noreply.github.com>
2025-04-21 23:59:43 +0900

    Update script.js

--- contact/script.js
+++ contact/script.js
@@ -6,11 +6,3 @@ const { createApp } = Vue;
       }
     }
   }).mount('#iframecontainer');
-
-$(window).on('load', function() {
-  
-  var $widget = $('iframe#zawazawa-embed-animamman-1-e1,iframe#InlineFrameContact');
-  var $widgetContents = $widget.contents();
-  $widgetContents.find('head').append('<link href="https://animanman.github.io/contact/if.css" rel="stylesheet" type="text/css">');
-
-});
