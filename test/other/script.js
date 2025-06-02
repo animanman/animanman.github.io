311a3530a5c228721d2de25627a65c63ee5b573a
animanman <103096854+animanman@users.noreply.github.com>
2025-06-02 22:29:21 +0900

    Update script.js

--- test/other/script.js
+++ test/other/script.js
@@ -11,7 +11,7 @@ Vue.createApp({
                 {
                   title: "下書き保存ツール",
                   url: "https://animanman.github.io/test/other/anm-Draftsavingtool.js",
-                  installurl: "https://animanman.github.io/test/other/anm-Draftsavingtool.js",
+                  installurl: "https://animanman.github.io/test/other/anm-Draftsavingtool.user.js",
                   description: "下書き保存を保存したり、書き込む時に安価先のレスを書き込みフォームの上に表示したりします"
                 }
                 // 他のスクリプト情報をここに追加可能
