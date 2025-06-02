c0f57988a27c1cca59f5c2a03fc74634e9b24e3b
animanman <103096854+animanman@users.noreply.github.com>
2025-06-02 21:44:18 +0900

    Create anm-Draftsavingtool.user.js

---
+++ test/other/anm-Draftsavingtool.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         下書き保存機能(中継)
+// @namespace    https://animanman.github.io/
+// @version      
+// @description  自動で下書きを保存するスクリプト(中継)
+// @author       animanman
+// @match        
+// @grant        none
+// @downloadURL  https://animanman.github.io/test/other/anm-Draftsavingtool.js
+// @updateURL    https://animanman.github.io/test/other/anm-Draftsavingtool.user.js
+// ==/UserScript==
+
+/* スクリプト本体は downloadURL で読み込まれる */
