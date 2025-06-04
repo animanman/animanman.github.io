8d6a6fb9f0a47d38adea538b020fabba5f9ca072
animanman <103096854+animanman@users.noreply.github.com>
2025-06-04 20:07:10 +0900

    Update script.js

--- test/other/script.js
+++ test/other/script.js
@@ -1,25 +1,24 @@
-// after: test/other/script.js
-// ※Vue の読み込みは default.html で済んでいる想定です
 Vue.createApp({
-  // Jekyll の Liquid と競合しないように delimiters を変更
-  delimiters: ['[[', ']]'],
-  data() {
-    return {
-      scriptGroups: [
-        {
-          heading: "Tampermonkey 用スクリプト",
-          scripts: [
+      // Jekyllとの記法競合回避のためのデリミタ変更
+      delimiters: ['[[', ']]'],
+      data() {
+        return {
+          // 各セクションごとに、headingとその中のスクリプト情報を格納
+          scriptGroups: [
             {
-              title: "下書き保存ツール",
-              url: "https://animanman.github.io/test/other/anm-Draftsavingtool.js",
-              installurl: "https://animanman.github.io/test/other/anm-Draftsavingtool.user.js",
-              description: "下書き保存を保存したり、書き込む時に安価先のレスを書き込みフォームの上に表示したりします"
+              heading: "tampermonkey用スクリプト",
+              scripts: [
+                {
+                  title: "下書き保存ツール",
+                  url: "https://animanman.github.io/test/other/anm-Draftsavingtool.js",
+                  installurl: "https://animanman.github.io/test/other/anm-Draftsavingtool.user.js",
+                  description: "下書き保存を保存したり、書き込む時に安価先のレスを書き込みフォームの上に表示したりします"
+                }
+                // 他のスクリプト情報をここに追加可能
+              ]
             }
-            // ここに他のスクリプトを追加できます
+            // 他のグループがある場合は、ここにオブジェクトを追加
           ]
         }
-        // 他にセクション（group）があれば、ここにオブジェクトを追加
-      ]
-    };
-  }
-}).mount("#otherapp");
+      }
+    }).mount("#otherapp")
