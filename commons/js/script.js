1b45c021040c24ae277a7f0b025474e00afec697
animanman <103096854+animanman@users.noreply.github.com>
2025-06-04 20:11:37 +0900

    Update script.js

--- commons/js/script.js
+++ commons/js/script.js
@@ -1,53 +1,44 @@
-// commons/js/script.js
-
-// ====================
-// ヘッダー用 Vue アプリ
-// ====================
 const headerapp = Vue.createApp({
-  // Jekyll と競合しないようにデリミタを [[ で変更
   delimiters: ['[[', ']]'],
   data() {
     return {
-      navOpen: false,
-      // headerLinks を手動で定義しているが、
-      // Jekyll 側で site.navigation を流し込みたければ Liquid で書き換え可能
       headerLinks: [
-        { text: 'ホーム',     url: '/' },
-        { text: 'About',    url: '/about/' },
-        { text: 'テスト版',  url: '/test/' },
-        { text: 'その他',    url: '/test/other/' },
+        { text: 'ホーム',    url: '/' },
+        { text: 'About',     url: '/about' },
+        { text: 'テスト版',  url: '/test' },
+        { text: 'その他',    url: '/test/other' },
         { text: 'お問い合わせ', url: 'https://zawazawa.jp/animamman/topic/1' }
-      ]
-    };
+      ],
+      navOpen: false   // ← ここでメニュー開閉用のフラグを持たせる
+    }
   },
   methods: {
     toggleNav() {
-      this.navOpen = !this.navOpen;
+      this.navOpen = !this.navOpen
+
       this.$nextTick(() => {
         const ul = this.$refs.navList;
-        if (ul) {
-          ul.style.maxHeight = this.navOpen
-            ? ul.scrollHeight + 'px'
-            : '0';
-        }
+        ul.style.maxHeight = this.navOpen
+          ? ul.scrollHeight + 'px'
+          : '0';
       });
     }
   }
-});
-headerapp.mount('#headerapp');
+})
+headerapp.mount('#headerapp')
 
-// ====================
-// フッター用 Vue アプリ
-// ====================
+
 const footerapp = Vue.createApp({
-  delimiters: ['[[', ']]'],
+  // Jekyllとの記法競合回避のためのデリミタ変更
+      delimiters: ['[[', ']]'],
   data() {
     return {
+      // 例：フッター用のデータ
       footerLinks: [
         { text: 'animanman.github.io', url: '/' },
-        { text: 'GitHub',             url: 'https://github.com/animanman/animanman.github.io/' }
+        { text: 'github', url: 'https://github.com/animanman/animanman.github.io/' }
       ]
-    };
+    }
   }
 });
 footerapp.mount('#footerapp');
